486b39cada969581ed4f23256bc99bedc50c4042
Lonadels <lonadels@ya.ru>
2024-03-16 22:29:00 +0300

    ass fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ dotenv.config(); // ALWAYS BE FIRST!
 
 import { autoRetry } from "@grammyjs/auto-retry";
 import { ParseModeFlavor, hydrateReply } from "@grammyjs/parse-mode";
-import { Context, Bot } from "grammy";
+import { Context, Bot, BotError, GrammyError, HttpError } from "grammy";
 import { Menu } from "@grammyjs/menu";
 
 export type BotContext = ParseModeFlavor<Context>;
@@ -23,12 +23,15 @@ let assOpen: boolean = false;
 const assStatus = () =>
   `*Статус жопы:* Жопа ${assOpen ? "открыта" : "закрыта"}`;
 
-const menu = new Menu("mainMenu").text(
-  () => (assOpen ? "Открыть жопу" : "Закрыть жопу"),
+const menu = new Menu("mainMenu", { autoAnswer: false }).text(
+  () => (assOpen ? "Закрыть жопу" : "Открыть жопу"),
   (ctx) => {
     assOpen = !assOpen;
     ctx.editMessageText(assStatus(), { parse_mode: "MarkdownV2" });
     ctx.menu.update();
+    ctx.answerCallbackQuery({
+      text: `Жопа ${assOpen ? "открыта" : "закрыта"}`,
+    });
   }
 );
 
@@ -44,5 +47,20 @@ bot.command("start", (ctx) => {
 bot.command("ass", (ctx) => {
   ctx.reply(assStatus(), { reply_markup: menu, parse_mode: "MarkdownV2" });
 });
+
+function errorHandler(err: BotError<BotContext>) {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+}
+
+bot.catch(errorHandler);
 
 bot.start();
