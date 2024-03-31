ea01afe00c9fd1147d00cfd282bdcd8c9cd08277
Lonadels <lonadels@ya.ru>
2024-03-31 05:58:07 +0300

    cleanup

--- src/commands/voice.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from "grammy";
-import { useType } from "../hooks/useType";
-import checkHasArgs from "../utils/checkHasArgs";
-import { GPTModule } from "../modules/GPTModule";
-
-export default function voice(gptModule: GPTModule) {
-  return () => gptModule.voice;
-}
