ba86c5c92b18a48a63837e57bd4d8e48956c6826
Lonadels <lonadels@ya.ru>
2024-04-03 04:25:47 +0300

    sizes

--- src/database/ImageTypes.ts
+++ src/database/ImageTypes.ts
@@ -6,8 +6,8 @@ export enum ImageQuality {
 export type ChatImageQuality = "standard" | "hd";
 
 export enum ImageSize {
-  Low = "256x256",
-  Medium = "512x512",
+  // Low = "256x256",
+  // Medium = "512x512",
   High = "1024x1024",
   Vertical = "1792x1024",
   Horizontal = "1024x1792",
